fb8aa255f8ac14d8c19353eabb4c8d87c808e2d5
spidergwin <godfreyjn873@gmail.com>
2026-05-11 10:57:26 +0100

    fix prisma config issues

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,2 +1,12 @@
-// Overwritten by JSON config
-export default {};
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations"
+  },
+  datasource: {
+    url: process.env.DATABASE_URL
+  },
+});
