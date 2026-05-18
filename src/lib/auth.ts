b38cf0d179dca526ffbcfe38eafcba60ddd5f48f
spidergwin <godfreyjn873@gmail.com>
2026-05-18 17:04:49 +0100

    added expo plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db";
+import { expo } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
   }),
+  plugins: [expo()],
   secret: process.env.BETTER_AUTH_SECRET!,
   trustedOrigins: [
     "http://localhost:3000",
