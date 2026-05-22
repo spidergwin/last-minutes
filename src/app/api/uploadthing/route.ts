5cefdda8a9c7ad8b61864ba49138d9c5bdbd2122
spidergwin <godfreyjn873@gmail.com>
2026-05-22 16:23:16 +0100

    fic ts errors

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -4,9 +4,4 @@ import { ourFileRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  // Apply global config if provided via env vars
-  config: {
-    uploadthingId: process.env.UPLOADTHING_APP_ID,
-    uploadthingSecret: process.env.UPLOADTHING_SECRET,
-  },
 });
